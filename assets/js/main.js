8cc08adf98c429f9fe01b089ef697eb06262124c
Stephanie Tran <stran40@myseneca.ca>
2021-01-05 16:15:47 -0500

    Updated from Publii

--- assets/js/main.js
+++ assets/js/main.js
@@ -216,42 +216,9 @@
 				}
 
 			// Hide intro on scroll (> small).
-				breakpoints.on('>small', function() {
+			
 
-					$main.unscrollex();
-
-					$main.scrollex({
-						mode: 'bottom',
-						top: '25vh',
-						bottom: '-50vh',
-						enter: function() {
-							$intro.addClass('hidden');
-						},
-						leave: function() {
-							$intro.removeClass('hidden');
-						}
-					});
-
-				});
-
-			// Hide intro on scroll (<= small).
-				breakpoints.on('<=small', function() {
-
-					$main.unscrollex();
-
-					$main.scrollex({
-						mode: 'middle',
-						top: '15vh',
-						bottom: '-15vh',
-						enter: function() {
-							$intro.addClass('hidden');
-						},
-						leave: function() {
-							$intro.removeClass('hidden');
-						}
-					});
-
-			});
+		
 
 		}
 
